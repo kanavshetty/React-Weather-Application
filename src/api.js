5acb0a24c23aa8c813119392ef2a40ebd2c8f7bf
Kanav Shetty <shettykv@mail.uc.edu>
2024-02-06 02:39:41 -0500

    Final Project

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '7e26c31b0emsha013e81cfbeb098p1cbc80jsn86c22c4ec6d8',
+		'X-RapidAPI-Key': '',  //Enter your Rapid API Key here to access cit locations
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,5 +9,5 @@ export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const Weather_API_KEY = "8af181eb141c92e891862e84fb1b2de9";
+export const Weather_API_KEY = ""; // Enter your OpenWeather API Key here.
 
